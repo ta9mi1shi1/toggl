9eecd3ff564627df2b2f9f4dc2a545f22c8d0749
Takumi Ishii <ta9mi1shi1@gmail.com>
2021-03-04 00:52:10 +0900

    Enable to use flags '--help' and '--version'

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ta9mi1shi1/toggl
+
+go 1.16
+
+require github.com/spf13/cobra v1.1.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ta9mi1shi1/toggl/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
